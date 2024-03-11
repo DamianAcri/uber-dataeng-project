556176b3133a560d3df53238f19ea92bd11fc8cc
Damián Acri <114292105+damitoccc@users.noreply.github.com>
2024-03-11 13:05:11 +0100

    uber_data_transformer.py

--- mageai-files/uber_data_transformer.py
+++ mageai-files/uber_data_transformer.py
@@ -8,11 +8,6 @@ if 'test' not in globals():
 @transformer
 def transform(df, *args, **kwargs):
     """
-    Template code for a transformer block.
-
-    Add more parameters to this function if this block has multiple parent blocks.
-    There should be one parameter for each output variable from each parent block.
-
     Args:
         data: The output from the upstream parent block
         args: The output from any additional upstream blocks (if applicable)
